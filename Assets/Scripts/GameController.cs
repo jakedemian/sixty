28fc4d4dc10b1c02fffe46fc6dd5d9905483c12e
Jake Demian <jakedemian@gmail.com>
2017-04-26 23:30:11 -0400

    updated player reset when player leaves camera view, and added UI Text to show the timer

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	private const float TIMER_AMOUNT_IN_SECONDS = 3f;
+	private const float TIMER_AMOUNT_IN_SECONDS = 10f;
 	private float timer = TIMER_AMOUNT_IN_SECONDS;
 	private Vector2 playerStartPosition;
 
 	public GameObject player;
+	public Text timerTextUI;
+
+	private Camera mainCamera;
+	private Plane[] planes;
+	private Collider2D playerCollider;
 
 	void Start(){
 		playerStartPosition = player.transform.position;
+
+		mainCamera = Camera.main;
+		planes = GeometryUtility.CalculateFrustumPlanes(mainCamera);
+        playerCollider = player.GetComponent<Collider2D>();
 	}
 
 	void Update(){
 		// if player falls out of camera view, reset
-		if(!player.GetComponent<Renderer>().isVisible){
+		planes = GeometryUtility.CalculateFrustumPlanes(mainCamera);
+		if(!GeometryUtility.TestPlanesAABB(planes, playerCollider.bounds)){
 			resetPlayer();
 		}
 
@@ -28,6 +39,10 @@ public class GameController : MonoBehaviour {
 
 	void FixedUpdate(){
 		timer -= Time.deltaTime;
+	}
+
+	void OnGUI(){
+		timerTextUI.text = timer.ToString("F2");
 	}
 
 	public void resetPlayer(){
