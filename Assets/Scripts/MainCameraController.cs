12d628dd860e87af9ce58aaeae3dd97dcf84128e
Jake Demian <jakedemian@gmail.com>
2017-04-26 23:59:56 -0400

    camera is now fixed to player, cannot go below y=0

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour {
+
+	public GameObject target;
+
+	void Start () {
+		transform.position = new Vector3(
+			target.transform.position.x, 
+			target.transform.position.y,
+			transform.position.z
+		);
+	}
+	
+	void Update () {
+		float newY = target.transform.position.y;
+		if(newY < 0f){
+			newY = 0f;
+		}
+
+		transform.position = new Vector3(
+			target.transform.position.x, 
+			newY,
+			transform.position.z
+		);
+	}
+}
