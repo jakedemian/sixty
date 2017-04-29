0b5210161e52a96e596b2b24e87d60c0ef514f9f
Jake Demian <jakedemian@gmail.com>
2017-04-29 11:56:32 -0400

    changed level and light position

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -14,7 +14,11 @@ public class MainCameraController : MonoBehaviour {
 			transform.position.z
 		);
 
-		mainLight.transform.position = transform.position;
+		mainLight.transform.position = new Vector3(
+			target.transform.position.x, 
+			target.transform.position.y,
+			transform.position.z
+		);
 	}
 	
 	void Update () {
@@ -29,6 +33,10 @@ public class MainCameraController : MonoBehaviour {
 			transform.position.z
 		);
 
-		mainLight.transform.position = transform.position;
+		mainLight.transform.position = new Vector3(
+			target.transform.position.x, 
+			target.transform.position.y,
+			transform.position.z
+		);
 	}
 }
